79e7433e2ea0446e79a37061bc1b6f70854e914c
johnchat <524267140@qq.com>
2015-12-21 23:27:00 +0800

    update for server

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ var config = {
     "env":"production",
     "appName": "quirkshop",
     //端口号配置
-    "port": 3000,
+    "port": 9006,
     //模板所在的目录
     "viewDir": path.join(__dirname,'..','view'),
     //log所在的目录
